631c146871f4e78845093e8e030f514520bde6f0
Hu Tao <hutao@supremind.com>
2020-08-11 16:07:05 +0800

    常规刷题

---
+++ UnionSet.cpp
@@ -0,0 +1,26 @@
+//
+// Created by tao hu on 2020/7/6.
+//
+#include <vector>
+using namespace std;
+
+class UnionSet(){
+  vector<int> root;
+  UnionSet(int n){
+    root=vector<int>(n);
+    for(int i=0;i<n;i++)
+      root[i]=i;
+  }
+
+  int find(int index){
+    return root[index]==index?index:root[index]=find(root[index]);
+  }
+
+  void Union(int x,int y){
+    int rx=find(x);
+    int ry=find(y);
+    if(rx!=ry){
+      root[rx]=ry;
+    }
+  }
+}
